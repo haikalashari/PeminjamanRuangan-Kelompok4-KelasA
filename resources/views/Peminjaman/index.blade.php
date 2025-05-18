406019154f5834b1feb24f46660787eb80ef30d4
nazwan74 <muhnazwanfadhilah74@gmail.com>
2025-05-18 16:29:49 +0700

    penambahan tombol kembali di riwayat peminjaman

--- resources/views/Peminjaman/index.blade.php
+++ resources/views/Peminjaman/index.blade.php
@@ -16,7 +16,12 @@
             </ul>
         </div>
     @endif
-    <h1>Data Peminjaman</h1>
+    <div class="d-flex justify-content-between align-items-center mb-4">
+        <h1>Data Peminjaman</h1>
+        <a href="{{ route('home') }}" class="btn btn-primary">
+            <i class="bi bi-arrow-left me-2"></i>Kembali
+        </a>
+    </div>
 
     <section class="section">
         @if(Auth::user()->admin)
